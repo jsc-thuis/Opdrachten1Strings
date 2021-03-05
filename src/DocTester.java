4c5b34518537af6fc44a052ac5e27339e9ca7e94
JSC <jozef.casteels@thomasmore.be>
2021-03-05 21:37:44 +0100

    Opdrachten1Strings Opgaven

---
+++ src/DocTester.java
@@ -0,0 +1,31 @@
+public class DocTester {
+
+    public DocTester() {
+    }
+
+    public void printLowerUpperCase(String woord) {
+    }
+
+    public String schrapLeestekensSpaties(String zin) {
+        return null;
+    }
+
+    public void printKlinkerSpatie(String woord) {
+    }
+
+    public boolean isPalindroom(String woord) {
+        return false;
+    }
+
+    public void printWoorden(String zin) {
+    }
+
+    public float printGemiddeldAantalLetters(String[] woorden) {
+        return 0.0F;
+    }
+
+    public void printNamenAlfabetisch(String[] namen) {
+    }
+}
+
+
---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        DocTester dt = new DocTester();
+        System.out.println("We testen de methodes uit de klasse DocTester... ");
+    }
+}
